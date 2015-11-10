f2dd12f06ae24cf3e362a20e8f058bad630d1ceb
Matthew Kim <matthewkim93@gmail.com>
2015-11-10 14:57:41 -0500

    Added swig templating features and basic template example

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var swig = require('swig');
 var express = require( 'express' );
 var app = express();
 var port = 3000;
@@ -7,16 +8,30 @@ app.use(function(req,res,next) {
 	next();
 })
 
+var data = { title: "An Example",
+ 	people: [
+ 		{ name: "Gandalf" },
+ 		{ name: "Frodo"},
+ 		{ name: "Hermione"}
+ ]};
+
 app.get("/", function(req, res, next) {
-	res.send("Hello World");
+	res.render( 'index', data)
 });
 
 app.get("/news", function(req, res, next) {
 	res.send("This is the news page.");
 });
 
+app.engine("html", swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+swig.setDefaults({ cache: false });
 
 
+swig.renderFile(__dirname + "/views/index.html", data, function(err, output) {
+	console.log(output);
+});
 
 app.listen(port, function() {
 	console.log("server listening");
